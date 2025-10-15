d9f5cb00223be4d0408dea3b401d9a195d7b5c9a
Monisha S <s.monisha060@gmail.com>
2025-10-15 21:06:26 +0530

    Update main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings: ["Frontend  Developer","web Developer","Data Science"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
